bdd795c31c60190c280a970da710fd5a3b64878a
Daniela Costa <costadanielagomes@gmail.com>
2024-10-13 16:01:08 +0100

    first commit

---
+++ snake/src/io/codeforall/fanstatics/Game.java
@@ -0,0 +1,16 @@
+package io.codeforall.fanstatics;
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
